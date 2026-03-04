613c1d59afb97b7405e36132f5777e3a8238a8ba
SivaDev <42565813+siva1b3@users.noreply.github.com>
2026-03-04 11:07:21 +0530

    aaa

---
+++ base-setup.sh
@@ -0,0 +1,32 @@
+sudo apt update
+
+sudo apt install -y \
+git \
+python3 \
+python3-pip \
+python3-venv \
+python3-dev
+
+
+sudo apt install -y \
+build-essential \
+curl \
+wget \
+vim \
+nano \
+htop \
+net-tools \
+unzip \
+zip \
+ca-certificates \
+software-properties-common \
+lsb-release \
+tree \
+jq
+
+
+git --version
+python3 --version
+pip3 --version
+
+
---
+++ nvidia-setup.sh
@@ -0,0 +1,8 @@
+
+sudo apt update
+sudo apt install -y nvidia-driver-570-server
+
+
+lsmod | grep nvidia
+
+
